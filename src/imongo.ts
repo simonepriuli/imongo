14e4ce968b4b5bf73f11921cb2850e125dca76de
2dayevents <simone.priuli@icloud.com>
2022-12-13 16:19:45 +0100

    remove unused params on imongo class

--- src/imongo.ts
+++ src/imongo.ts
@@ -2,8 +2,6 @@ import * as mongoDB from 'mongodb';
 
 export class imongo {
   private db!: mongoDB.Db;
-  //private _collection: mongoDB.Collection<mongoDB.Document> | undefined;
-  //private _collection: mongoDB.Collection<mongoDB.Document> | undefined;
   private collections: Map<string, mongoDB.Collection<mongoDB.Document>> =
     new Map([]);
 
